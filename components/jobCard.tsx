ea278577a30e9d22f7a2051d03b0bbccd5e84657
genius-ayush <ayushsanjayrawal@gmail.com>
2025-01-10 14:51:32 +0530

    10 jan 14:51:30

--- components/jobCard.tsx
+++ components/jobCard.tsx
@@ -6,6 +6,8 @@ import { Landmark, MapPin } from 'lucide-react';
 import Link from 'next/link';
 import { Button } from "@/components/ui/button"
 import { Minus, Plus } from "lucide-react"
+import { ScrollArea } from "@/components/ui/scroll-area"
+
 import {
   Drawer,
   DrawerClose,
@@ -90,93 +92,100 @@ interface JobPostInterface {
 function DateFormatter({ isoDate }) {
   const date = new Date(isoDate);
 
-  
+
   const options: Intl.DateTimeFormatOptions = {
     year: "numeric",
     month: "long",
     day: "numeric",
   };
-  
+
   const formattedDate = date.toLocaleDateString("en-US", options);
 
   return <span>{formattedDate}</span>;
 }
 
 //@ts-ignore
-export default function JobCard({job}: JobPostInterface) {
+export default function JobCard({ job }: JobPostInterface) {
   const [goal, setGoal] = React.useState(350)
-  console.log(job) ;
-  console.log(job.body) 
-  
+  console.log(job);
+  console.log(job.body)
+
   function onClick(adjustment: number) {
     setGoal(Math.max(200, Math.min(400, goal + adjustment)))
   }
   return (
     <div>
-    {/* <Link href={`/jobs/1`}> */}
       <Drawer >
-      <DrawerTrigger asChild>
-      <div className="border  transition-all duration-115 ease-linear hover:bg-lightBgSecondary dark:hover:bg-darkBgSecondary flex flex-col gap-6 h-fit max-h-[10rem] p-4 rounded-xl">
-        <div className="flex gap-4 items-center">
-          <div className="w-[4rem] h-[4rem]  rounded-md">
-            
-              <Image
-                className="size-full object-contain"
-                src={job.companyImage.url}
-                width={'500'}
-                height={'500'}
-                alt="company-logo"
-              />
-          </div>
-          <div className="flex flex-col gap-2">
-            <h2 className="font-bold">{job.title}</h2>
-            <div className="text-xs flex gap-1 font-medium items-center text-gray-500">
-              <span>{job.jobTypeReference.jobType}</span>•
-              <span>Posted on  <DateFormatter isoDate={job.createdAt}/>  </span>
+        <DrawerTrigger asChild>
+          <div className="border  transition-all duration-115 ease-linear hover:bg-lightBgSecondary dark:hover:bg-darkBgSecondary flex flex-col gap-6 h-fit max-h-[10rem] p-4 rounded-xl">
+            <div className="flex gap-4 items-center">
+              <div className="w-[4rem] h-[4rem]  rounded-md">
+
+                <Image
+                  className="size-full object-contain"
+                  src={job.companyImage.url}
+                  width={'500'}
+                  height={'500'}
+                  alt="company-logo"
+                />
+              </div>
+              <div className="flex flex-col gap-2">
+                <h2 className="font-bold">{job.title}</h2>
+                <div className="text-xs flex gap-1 font-medium items-center text-gray-500">
+                  <span>{job.jobTypeReference.jobType}</span>•
+                  <span>Posted on  <DateFormatter isoDate={job.createdAt} />  </span>
+                </div>
+              </div>
+            </div>
+            <div className="flex items-center gap-4">
+              <div className="px-4 py-1 w-fit text-blue-500 bg-blue-500/20 rounded-lg flex items-center justify-center text-xs font-bold">
+
+                {job.domain.domain}
+              </div>
+              <div className="flex items-center gap-2">
+                <Landmark size={16} />
+                <p className="text-xs font-semibold">
+                  {job.salary}
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="flex items-center gap-4">
-          <div className="px-4 py-1 w-fit text-blue-500 bg-blue-500/20 rounded-lg flex items-center justify-center text-xs font-bold">
-            
-           {job.domain.domain}
-          </div>
-          <div className="flex items-center gap-2">
-            <Landmark size={16}/>
-            <p className="text-xs font-semibold">
-            {job.salary}
-            </p>
-          </div>
-        </div>
-      </div>
-      </DrawerTrigger>
-      <DrawerContent className=''>
-        <div className="mx-auto w-2/3  ">
-          <DrawerHeader>
-            <DrawerTitle>{job.title}</DrawerTitle>
-            <DrawerDescription>Set your daily activity goal.</DrawerDescription>
-          </DrawerHeader>
-          {/* <div className="p-4 pb-0">
-            <div className="flex items-center justify-center space-x-2"> */}
-              
-            <div dangerouslySetInnerHTML={{ __html: job.body.html }} />
-              
-                
+        </DrawerTrigger>
+        <DrawerContent className='h-2/3'>
+          <ScrollArea className="h-full w-full rounded-md  p-4">
+            <div className="mx-auto w-4/5  ">
+              <DrawerHeader>
+                <DrawerTitle className='font-bold text-2xl'>{job.title}</DrawerTitle>
+                <DrawerDescription className='flex gap-2'><div className="px-4 py-1 w-fit text-blue-500 bg-blue-500/20 rounded-lg flex items-center justify-center text-xs font-bold">
+
+                  {job.domain.domain}
+                </div>
+                <div className="px-4 py-1 w-fit text-blue-500 bg-blue-500/20 rounded-lg flex items-center justify-center text-xs font-bold">
+
+                  {job.jobTypeReference.jobType}
+                </div>
+                <div className="px-4 py-1 w-fit text-blue-500 bg-blue-500/20 rounded-lg flex items-center justify-center text-xs font-bold">
+
+                  {job.salary}
+                </div>
+                </DrawerDescription>
+              </DrawerHeader>
+              <div className="p-4 pb-0">
+                <div className="flex items-center justify-center space-x-2">
+
+                  <div dangerouslySetInnerHTML={{ __html: job.body.html }} /></div>
+              </div>
+              <DrawerFooter className='w-32'>
+                <Button>Apply</Button>
+                <DrawerClose asChild>
+                  <Button variant="outline">Cancel</Button>
+                </DrawerClose>
+              </DrawerFooter>
             </div>
-            {/* <div className="mt-3 h-[120px]">
-              hey 
-            </div> */}
-          {/* </div> */}
-          {/* <DrawerFooter>
-            <Button>Submit</Button>
-            <DrawerClose asChild>
-              <Button variant="outline">Cancel</Button>
-            </DrawerClose>
-          </DrawerFooter> */}
-        {/* </div> */}
-      </DrawerContent>
+
+          </ScrollArea>
+        </DrawerContent>
       </Drawer>
-    {/* </Link> */}
     </div>
   );
 }
